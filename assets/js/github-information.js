d1077f88a6ef2317347ee9ada3efa9b95ff7f766
RobWar-code <rowaniar@aol.com>
2023-02-15 18:57:56 +0000

    Add repository information function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,6 +16,31 @@ function userInformationHTML(user){
     `;
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length === 0) {
+        return `
+            <div class="clearfix repo-list">No Repos!</div>
+        `;
+    }
+
+    var listItemsHTML = repos.map(repo => {
+        return `
+            <li>
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </li>
+        `;
+    });
+
+    return `
+        <div class="clearfix repo-list">
+            <p><strong>Repo List:</strong></p>
+            <ul>
+                ${listItemsHTML.join('\n')}
+            </ul>
+        </div>
+    `;
+}
+
 function fetchGitHubInformation(event) {
     var username = $("#gh-username").val();
     if (!username) {
@@ -30,11 +55,14 @@ function fetchGitHubInformation(event) {
     );
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then (
-        response => {
-            var userData = response;
+        (firstResponse, secondResponse) => {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         errorResponse => {
             if (errorResponse.status === 404) {
