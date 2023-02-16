d22f312cc2353e64e6192afe83fe81ac9aceb1f5
RobWar-code <rowaniar@aol.com>
2023-02-16 10:12:49 +0000

    Clear github display before load, display initial repo on ready

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -42,6 +42,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html("<h2>Please Enter Your Username</h2>");
@@ -76,3 +79,5 @@ function fetchGitHubInformation(event) {
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
